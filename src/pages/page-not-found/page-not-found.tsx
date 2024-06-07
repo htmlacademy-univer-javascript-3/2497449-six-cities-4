699d5bd2299b05278b48c9c7f4d2f195bc40cd0d
София Семенова <sofisem@yandex.ru>
2024-06-07 22:29:27 +0300

    fixes

--- src/pages/page-not-found/page-not-found.tsx
+++ src/pages/page-not-found/page-not-found.tsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const';
 
 function PageNotFound(): JSX.Element {
   return (
     <div className="container">
       <h2>404 error</h2>
-      <Link to={'/'}>Go to Main Page</Link>
+      <Link to={AppRoute.Main}>Go to Main Page</Link>
     </div>
   );
 }
